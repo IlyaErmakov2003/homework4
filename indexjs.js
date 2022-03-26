d068222147f51d6c2aca38790afbac8f1094b8da
IlyaErmakov2003 <https://github.com/IlyaErmakov2003/homework4.git>
2022-03-26 18:32:09 +0300

    homework4

---
+++ indexjs.js
@@ -0,0 +1,216 @@
+function  sumDeclaration(a, b){
+    const result = a + b;
+    console.log(result);
+}
+sumDeclaration(5,8);
+//function Declaration
+
+const sumExpression = function (a, b){
+    const result = a + b;
+    console.log(result);
+}
+sumExpression(8 ,9);
+//function Expression
+
+
+const sumArrow = (a, b)=>a+b
+console.log(sumArrow(2,3));
+//function Arrow
+
+function subDeclaration(a, b){
+    const result = a - b ;
+    console.log(result);
+}
+subDeclaration(8 , 5)
+//function Declaration
+
+const subExpression = function (a, b){
+    const result = a - b;
+    console.log(result);
+}
+subExpression(11 ,1);
+//function Expression
+
+const subArrow = (a, b)=>a-b
+console.log(subArrow(5,3));
+//function Arrow
+
+function  mulDeclaration(a, b){
+    const result = a * b;
+    console.log(result);
+}
+mulDeclaration(5,8);
+//function Declaration
+
+const mulExpression = function (a, b){
+    const result = a * b;
+    console.log(result);
+}
+mulExpression(9 ,9);
+//function Expression
+
+const mulArrow = (a, b)=>a*b
+console.log(mulArrow(3,3));
+//function Arrow
+
+function  divDeclaration(a, b){
+    const result = a / b;
+    console.log(result);
+}
+divDeclaration(40,5);
+//function Declaration
+
+const divExpression = function (a, b){
+    const result = a / b;
+    console.log(result);
+}
+divExpression(49 ,7);
+//function Expression
+
+const divArrow = (a, b)=>a/b
+console.log(divArrow(36,6));
+//function Arrow
+
+
+function  edgeDeclaration(a, b){
+    const result = a % b;
+    console.log(result);
+}
+edgeDeclaration(48,9);
+//function Declaration
+
+const edgeExpression = function (a, b){
+    const result = a % b;
+    console.log(result);
+}
+edgeExpression(10 ,4);
+//function Expression
+
+const edgeArrow = (a, b)=>a%b
+console.log(edgeArrow(8,7));
+//function Arrow
+
+
+function  moreDeclaration(a, b){
+    const result = a > b;
+    console.log(result);
+}
+moreDeclaration(5,8);
+//function Declaration
+
+const moreExpression = function (a, b){
+    const result = a > b;
+    console.log(result);
+}
+moreExpression(11 ,9);
+//function Expression
+
+
+const moreArrow = (a, b)=>a>b
+console.log(moreArrow(8,3));
+//function Arrow
+
+
+function  lessDeclaration(a, b){
+    const result = a < b;
+    console.log(result);
+}
+lessDeclaration(8,1);
+//function Declaration
+
+
+const lessExpression = function (a, b){
+    const result = a < b;
+    console.log(result);
+}
+lessExpression(55 ,80);
+//function Expression
+
+
+const lessArrow = (a, b)=>a<b
+console.log(lessArrow(7,3));
+//function Arrow
+
+
+function  moreeqDeclaration(a, b){
+    const result = a >= b;
+    console.log(result);
+}
+moreeqDeclaration(8,8);
+//function Declaration
+
+
+const moreeqExpression = function (a, b){
+    const result = a >= b;
+    console.log(result);
+}
+moreeqExpression(11 ,9);
+//function Expression
+
+
+const moreeqArrow = (a, b)=>a>=b
+console.log(moreeqArrow(8,5));
+//function Arrow
+
+
+function  lesseqDeclaration(a, b){
+    const result = a <= b;
+    console.log(result);
+}
+lesseqDeclaration(2,8);
+//function Declaration
+
+
+const lesseqExpression = function (a, b){
+    const result = a <= b;
+    console.log(result);
+}
+lesseqExpression(4 ,9);
+//function Expression
+
+
+const lesseqArrow = (a, b)=>a<=b
+console.log(lesseqArrow(88,55));
+//function Arrow
+
+
+function  abseqDeclaration(a, b){
+    const result = a === b;
+    console.log(result);
+}
+abseqDeclaration(7,7);
+//function Declaration
+
+
+const abseqExpression = function (a, b){
+    const result = a === b;
+    console.log(result);
+}
+abseqExpression(15 ,9);
+//function Expression
+
+
+const abseqArrow = (a, b)=>a===b
+console.log(abseqArrow(7 , 18));
+//function Arrow
+
+
+function  noteqDeclaration(a, b){
+    const result = a !== b;
+    console.log(result);
+}
+noteqDeclaration(17,5);
+//function Declaration
+
+
+const noteqExpression = function (a, b){
+    const result = a !== b;
+    console.log(result);
+}
+noteqExpression(15 ,7);
+//function Expression
+
+
+const noteqArrow = (a, b)=>a!==b
+console.log(noteqArrow(11,25));
+//function Arrow
